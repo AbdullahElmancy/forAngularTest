db0b4660b595d240515cc9de9478d609430ecd84
Abdullah Elmancy <154111274+AbdullahElmancy@users.noreply.github.com>
2024-05-01 16:58:34 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const cors = require("cors")
 const tetsConnection = require('./DB/connection')
 const { userRouter } = require('./routes')
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT
+cors({
+    origin: "*",
+    optionsSuccessStatus:200
+})
 tetsConnection()
 app.use(express.json())
 app.use(userRouter)
